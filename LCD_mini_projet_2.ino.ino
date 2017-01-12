3357b010ef35387b2014c4904559d67a6fd864bb
ChristineBoutin <corentin.lapastoure@gmail.com>
2017-01-12 19:58:26 +0100

    Update LCD_mini_projet_2.ino.ino

--- LCD_mini_projet_2.ino.ino
+++ LCD_mini_projet_2.ino.ino
@@ -1,5 +1,5 @@
 #include <rgb_lcd.h>//Ajout de la bibliothèque du lcd
-#include <Wire.h> //Ajout de la biggggbliothèque i2c pour utiliser le lcd
+#include <Wire.h> //Ajout de la bibliothèque i2c pour utiliser le lcd
 rgb_lcd lcd;  //Constructeur rgb_lcd pour l'objet lcd
 
  char valeur ;// Variable servant à récupérer  les données reçues
@@ -8,21 +8,21 @@ rgb_lcd lcd;  //Constructeur rgb_lcd pour l'objet lcd
 void setup() //Initialisations
 {
     lcd.begin(16, 2); //Initialisation du lcd de 16 colonnes et 2 lignes
-    Serial.begin(9600); 
+    Serial.begin(9600); // initialisation série
     
  }
  
 
 void loop() //Programme principal
  {  
-   lcd.setRGB(0,0,0);
+   lcd.setRGB(0,0,0); // rétro éclairage eteint 
      if (Serial.available()>0);// Si des données sont disponibles
      {
         valeur = Serial.read(); // On les récupère
-         if(valeur == 'g')
+         if(valeur == 'g') // si variable = g
           {
-           Serial.println(valeur);
-           lcd.setRGB(0,255,0);
+           Serial.println(valeur); // afficher la variable 
+           lcd.setRGB(0,255,0);// 
            delay(1000);
           }  
               
